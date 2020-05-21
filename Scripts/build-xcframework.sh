cf7d30c41e9c1159f9b2d8242960e05a38023b16
Anastasia Kubova <v-ankubo@microsoft.com>
2020-05-21 15:26:51 +0300

    renaming, add configuration attribute and change paths in build xcframework script

--- Scripts/build-xcframework.sh
+++ Scripts/build-xcframework.sh
@@ -4,31 +4,29 @@
 # Licensed under the MIT License.
 
 # Work dir is directory where all XCFramework artifacts is stored.
+BUILD_DIR=build
 WORK_DIR="${SRCROOT}/../AppCenter-SDK-Apple/xcframework"
 
 # Work dir will be the final output to the framework.
 XC_FRAMEWORK_PATH="${WORK_DIR}/Output/${PROJECT_NAME}.xcframework"
 
-# Directory where catalyst store frameworks after build.
-CATALYST_BUILD_DIR="${WORK_DIR}/Release-maccatalyst/"
-
 # Clean previus XCFramework build.
 rm -rf ${PROJECT_NAME}.xcframework/
 
 # Build and move mac catalyst framework
-xcodebuild -project "${SRCROOT}/${PROJECT_NAME}.xcodeproj" -configuration "Release" -scheme "${PROJECT_NAME} iOS Framework" -destination 'platform=macOS,variant=Mac Catalyst' CONFIGURATION_BUILD_DIR="${CATALYST_BUILD_DIR}"
+xcodebuild -project "${SRCROOT}/${PROJECT_NAME}.xcodeproj" -configuration "${CONFIGURATION}" -scheme "${PROJECT_NAME} iOS Framework" -destination 'platform=macOS,variant=Mac Catalyst'
 
 # Create a command to build XCFramework.
 for SDK in iphoneos iphonesimulator appletvos appletvsimulator macOS maccatalyst; do
-    FRAMEWORK_PATH="$WORK_DIR/Release-$SDK/${PROJECT_NAME}.framework"
-    [ -e "$FRAMEWORK_PATH" ] && XC_BUILD_COMMAND="$XC_BUILD_COMMAND -framework $FRAMEWORK_PATH";
+    FRAMEWORK_PATH="${BUILD_DIR}/${CONFIGURATION}-${SDK}/${PROJECT_NAME}.framework"
+    [ -e "${FRAMEWORK_PATH}" ] && XC_BUILD_COMMAND="${XC_BUILD_COMMAND} -framework ${FRAMEWORK_PATH}";
 done
-XC_BUILD_COMMAND="xcodebuild -create-xcframework $XC_BUILD_COMMAND -output $XC_FRAMEWORK_PATH"
+XC_BUILD_COMMAND="xcodebuild -create-xcframework ${XC_BUILD_COMMAND} -output ${XC_FRAMEWORK_PATH}"
 
 #Build XCFramework
 eval "$XC_BUILD_COMMAND"
 
-RES_FILE_PATH="$WORK_DIR/Release-iphoneos/AppCenterDistributeResources.bundle"
+RES_FILE_PATH="${BUILD_DIR}/${CONFIGURATION}-iphoneos/AppCenterDistributeResources.bundle"
 if [[ ${PROJECT_NAME} == "AppCenterDistribute" ]] && [ -e "${RES_FILE_PATH}" ]; then
     mv "${RES_FILE_PATH}" "${XC_FRAMEWORK_PATH}"
 fi
